a4b200cbefcc38edd9f3a3213f6a1268aad81c90
ligang7138 <445123875@qq.com>
2019-06-03 10:28:34 +0800

    修改测试用例

--- storage/kafka/kafka_test.go
+++ storage/kafka/kafka_test.go
@@ -4,19 +4,20 @@ import (
 	"testing"
 )
 
-func newProducer() (*Producer,error) {
+func newProducer() (*Producer, error) {
 	address := []string{
 		"172.25.0.36:9092",
+		"172.25.0.37:9092",
 	}
-	p,err := NewProducer(address)
+	p, err := NewProducer(address)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return p,nil
+	return p, nil
 }
 
 func TestNewProducer(t *testing.T) {
-	p,err := newProducer()
+	p, err := newProducer()
 	if err != nil {
 		t.Error(err)
 	}
@@ -26,17 +27,21 @@ func TestNewProducer(t *testing.T) {
 }
 
 func TestProducer_SyncProducer(t *testing.T) {
-	p,err := newProducer()
+	p, err := newProducer()
 	if err != nil {
 		t.Error(err)
 	}
-	err = p.SyncProducer("test","TestProducer_SyncProducer")
+	err = p.SyncProducer("test", "TestProducer_SyncProducer")
 	if err != nil {
 		t.Error(err)
+	} else {
+		t.Log("pass!!!")
 	}
-	t.Log("pass!!!")
 }
 
 func TestProducer_AsyncProducer(t *testing.T) {
 
+	ch := make(chan int)
+
+	<-ch
 }
